e1ad401a72b09907f295ba047ea003280ebd3edf
ashankar12 <akshayshankar@hotmail.com>
2016-10-12 13:03:42 -0700

    [TEST] Introducing comments that were missing in first test file
    Improvement

--- gittest.py
+++ gittest.py
@@ -1,10 +1,42 @@
-Names added to same branch:
-Akshay Shankar
+#!/usr/bin/python
+
+# GIT TEST FILE
+
+'''
+Hi Guys, this is a quick file to just get used to this version and application of github.
+I think there are seriously only two options of git that we need to get faimliar with to
+utilize this resource.
+
+I chose python as I am confident that all of us have an IDE capable of running python scripts.
+
+We can use this file as quick tutorial of git.
+
+To do : Below you see two blocks of print statements. In the first block add you name and push
+		the code to the same branch.
+
+		For the second block, create a new branch, add you name and merge your code into the 
+		previous branch.
+
+		The final output of our efforts should look like this:
+
+		print 'Names added to same branch:'
+		print 'Akshay Shankar'
+		print 'Nam Cuong'
+		print 'Jordan Miller'
+
+		print 'Names merged in from different branch:'
+		print 'Shankar Akshay'
+		print 'Cuong Nam'
+		print 'Miller Jordan'
+'''
+
+print 'Names added to same branch:'
+print 'Akshay Shankar'
+print ''
+print '\n'
 
 
-
-Names merged in from different branch:
-Shankar Akshay
-
-
->>> 
+print 'Names merged in from different branch:'
+print 'Shankar Akshay'
+print ''
+print ''
